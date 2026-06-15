69846bc1c2fb07cb6fb50570743c5b056291e1b2
Evan Tung <dragonejt@gmail.com>
2026-06-15 00:59:08 -0400

    fix: add integration workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "8.6.0"
+    jacoco
+    id("org.sonarqube") version "7.3.1.8318"
 }
 
 group = "dev.dragonejt"
@@ -26,14 +28,41 @@ dependencies {
     implementation("dev.kord:kord-core:0.18.1")
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
+    implementation("io.sentry:sentry-spring-boot-4:8.43.2")
+    implementation("io.sentry:sentry-async-profiler:8.43.2")
 }
 
 kotlin { compilerOptions { freeCompilerArgs.addAll("-Xjsr305=strict") } }
 
-tasks.withType<Test> { useJUnitPlatform() }
+tasks.withType<Test> {
+    useJUnitPlatform() 
+
+    reports {
+        junitXml.required.set(true)
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
 
 spotless {
     kotlin {
         ktfmt("0.63").kotlinlangStyle()
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.organization", "dragonejt")
+        property("sonar.projectKey", "dragonejt_hakase")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property("sonar.exclusions", "**/build/**")
     }
 }
