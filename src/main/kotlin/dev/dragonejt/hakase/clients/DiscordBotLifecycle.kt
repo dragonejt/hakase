fd88dc12a5d3030629c3ae70a42e91619f3b1b8f
Evan Tung <dragonejt@gmail.com>
2026-07-03 23:01:03 -0400

    fix: make bot in DiscordBotLifecycle lateinit instead of nullable

--- src/main/kotlin/dev/dragonejt/hakase/clients/DiscordBotLifecycle.kt
+++ src/main/kotlin/dev/dragonejt/hakase/clients/DiscordBotLifecycle.kt
@@ -2,8 +2,7 @@ package dev.dragonejt.hakase.clients
 
 import dev.dragonejt.hakase.interactions.SlashCommand
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
+import java.util.concurrent.atomic.AtomicBoolean
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import org.springframework.context.SmartLifecycle
@@ -11,19 +10,21 @@ import org.springframework.stereotype.Component
 
 @Component
 class DiscordBotLifecycle(
-    private val scope: CoroutineScope,
     private val botConfig: JDABuilder,
     private val commands: List<SlashCommand>,
 ) : SmartLifecycle {
     private val log = KotlinLogging.logger {}
-    private var bot: JDA? = null
+    private lateinit var bot: JDA
+    private var running = AtomicBoolean(false)
 
     override fun start() {
-        log.info { "Starting Discord Bot..." }
+        if (isRunning()) return
 
+        log.info { "Starting Discord Bot..." }
         bot = botConfig.build()
-        bot!!.updateCommands().addCommands(commands.map { command -> command.command() }).queue()
-        bot!!.awaitReady()
+        bot.updateCommands().addCommands(commands.map { command -> command.command() }).queue()
+        bot.awaitReady()
+        running.set(true)
     }
 
     override fun stop() {
@@ -31,14 +32,16 @@ class DiscordBotLifecycle(
     }
 
     override fun stop(callback: Runnable) {
+        if (!isRunning()) return
+
         log.info { "Stopping Discord Bot..." }
-        bot!!.shutdown()
-        bot!!.awaitShutdown()
-        scope.cancel()
+        bot.shutdown()
+        bot.awaitShutdown()
+        running.set(false)
         callback.run()
     }
 
-    override fun isRunning(): Boolean = bot?.status == JDA.Status.CONNECTED
+    override fun isRunning(): Boolean = running.get()
 
     override fun isAutoStartup() = true
 }
