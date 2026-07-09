3176edabff3e18a465aec1590b1a8c567fff73e9
Evan Tung <dragonejt@gmail.com>
2026-07-09 19:22:36 -0400

    fix: actually use OntologyProperties in OntologyConfiguration

--- src/main/kotlin/dev/dragonejt/hakase/clients/OntologyConfiguration.kt
+++ src/main/kotlin/dev/dragonejt/hakase/clients/OntologyConfiguration.kt
@@ -4,28 +4,31 @@ import com.palantir.osdk.api.auth.ConfidentialClientAuth
 import com.palantir.osdk.internal.api.FoundryConnectionConfig
 import dev.dragonejt.hakase.telemetry.LogBase
 import dev.dragonejt.hakase_sdk.FoundryClient
-import dev.dragonejt.hakase_sdk.Ontology5a5029d53a2342fdA36a2cf9e72e73b9
+import dev.dragonejt.hakase_sdk.Ontology5a5029d53a2342fdA36a2cf9e72e73b9 as Ontology
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@ConfigurationProperties(prefix = "discord")
-data class OntologyProperties(val url: String, val token: String)
+@ConfigurationProperties(prefix = "ontology")
+data class OntologyProperties(val url: String, val clientID: String, val token: String)
 
 @Configuration
+@EnableConfigurationProperties(OntologyProperties::class)
 class OntologyConfiguration : LogBase() {
 
-    private val clientID = "33976368fd8b43e534fd9e4b0a51c4c0"
-
     @Bean
-    fun ontology(props: OntologyProperties): Ontology5a5029d53a2342fdA36a2cf9e72e73b9 {
+    fun ontology(props: OntologyProperties): Ontology {
         log.atDebug {
             message =
-                "Building Palantir Ontology SDK Client with URL: ${props.url} and client ID: $clientID"
-            payload = mapOf("foundry_uri" to props.url, "foundry_client_id" to clientID)
+                "Building Palantir Ontology SDK Client with URL: ${props.url} and client ID: ${props.clientID}"
+            payload = mapOf("foundry_uri" to props.url, "foundry_client_id" to props.clientID)
         }
         val auth =
-            ConfidentialClientAuth.builder().clientId(clientID).clientSecret(props.token).build()
+            ConfidentialClientAuth.builder()
+                .clientId(props.clientID)
+                .clientSecret(props.token)
+                .build()
         val connection = FoundryConnectionConfig.builder().foundryUri(props.url).build()
 
         return FoundryClient.builder().auth(auth).connectionConfig(connection).build().ontology()
