4882ffc01160a900a744801efed4c1f59873539b
AKY <55674447+dundun007@users.noreply.github.com>
2026-07-16 15:37:27 -0700

    added changes

--- src/main/kotlin/dev/dragonejt/hakase/events/ReadyHandler.kt
+++ src/main/kotlin/dev/dragonejt/hakase/events/ReadyHandler.kt
@@ -1,37 +1,24 @@
 package dev.dragonejt.hakase.events
 
-import dev.dragonejt.hakase.telemetry.LogBase
 import dev.minn.jda.ktx.events.CoroutineEventListener
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.opentelemetry.api.trace.SpanKind
 import io.opentelemetry.api.trace.Tracer
-import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import org.springframework.stereotype.Service
 
 @Service
-class ReadyHandler(private val tracer: Tracer) : CoroutineEventListener, LogBase() {
+class ReadyHandler(private val tracer: Tracer) : CoroutineEventListener {
+    private val log = KotlinLogging.logger {}
 
     override suspend fun onEvent(event: GenericEvent) {
         if (event !is ReadyEvent) return
 
-        val span =
-            tracer
-                .spanBuilder("events.${this.javaClass.simpleName}")
-                .setSpanKind(SpanKind.SERVER)
-                .startSpan()
+        val span = tracer.spanBuilder("events.ready").setSpanKind(SpanKind.SERVER).startSpan()
         val scope = span.makeCurrent()
 
-        log.atInfo {
-            message = "Logged in as ${event.jda.selfUser.name}!"
-            payload = mapOf("bot_user" to event.jda.selfUser.name)
-        }
-
-        event.jda.presence.activity =
-            Activity.of(
-                Activity.ActivityType.CUSTOM_STATUS,
-                "hakase.dragonejt.dev | in ${event.jda.guilds.size} courses",
-            )
+        log.info { "Logged in as ${event.jda.selfUser.name}!" }
 
         scope.close()
         span.end()
